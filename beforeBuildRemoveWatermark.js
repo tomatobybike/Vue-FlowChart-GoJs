cd5f3423f0350cb41b71b4265b2f08cef731c2a7
Tom <>
2023-02-14 14:41:03 +0800

    feat: 🎸 移除水印

---
+++ beforeBuildRemoveWatermark.js
@@ -0,0 +1,1 @@
+if(f.indexOf('GoJS 2.2 evaluation') > -1||f.indexOf('(c) 1998-2023 Northwoods Software') > -1||f.indexOf('Not for distribution or production use') > -1||f.indexOf('gojs.net') > -1){return '';}else{return f};
